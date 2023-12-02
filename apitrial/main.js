eb34ca091342bc0aaf1398f0410037d5a0db8134
vandertheprince <109791828+Vandertheprince@users.noreply.github.com>
2023-12-03 03:40:18 +0800

    Add files via upload

---
+++ apitrial/main.js
@@ -0,0 +1,25 @@
+const catImg = document.getElementById('catImage');
+const loaCAT = document.getElementById('learn-more');
+
+const URL_CAT = 'https://cataas.com/cat';
+
+const loaCATimages = () => {
+    fetch(URL_CAT)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.blob();
+        })
+        .then(blob => {
+            const imageURL = URL.createObjectURL(blob);
+            console.log(imageURL);
+            catImg.src = imageURL;
+        })
+        .catch(error => {
+            console.error('Error fetching cat image:', error);
+        });
+};
+
+// Adding event listener for button click
+loaCAT.addEventListener('click', loaCATimages);
